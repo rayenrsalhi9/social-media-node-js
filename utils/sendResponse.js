59eb30d8b140e06a468f27aa17432ea18c8a97e5
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-07-27 17:33:32 +0100

    feat(utils): add CORS headers to sendResponse function
    Enable cross-origin resource sharing by adding Access-Control-Allow-Origin and Access-Control-Allow-Methods headers to responses

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -1,5 +1,7 @@
 export function sendResponse(res, code, content, type) {
     res.statusCode = code
     res.setHeader('Content-Type', type)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT')
     res.end(content)
 }
